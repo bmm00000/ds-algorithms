9d9f06e5057f6b39dad88d737d0475a779147b11
bmm00000 <bmm00000@icloud.com>
2021-06-12 13:33:35 +0100

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -49,21 +49,55 @@ function steps(n) {
 	// }
 	//
 	//
+	// for (let row = 0; row < n; row++) {
+	// 	let string = '';
+	// 	for (let column = 0; column < n; column++) {
+	// 		if (column > row) {
+	// 			string += ' ';
+	// 		} else {
+	// 			string += '#';
+	// 		}
+	// 	}
+	// 	console.log(string);
+	// }
+	//
+	//
+	//
+	// for (let row = 0; row < n; row++) {
+	// 	let str = '';
+
+	// 	for (let column = 0; column < n; column++) {
+	// 		if (column > row) {
+	// 			str += ' ';
+	// 		} else {
+	// 			str += '#';
+	// 		}
+	// 	}
+
+	// 	console.log(str);
+	// }
+	//
+	//
+	//
 	for (let row = 0; row < n; row++) {
-		let string = '';
+		let str = '';
 
 		for (let column = 0; column < n; column++) {
-			if (column > row) {
-				string += ' ';
+			if (column <= row) {
+				str += '#';
 			} else {
-				string += '#';
+				str += ' ';
 			}
 		}
 
-		console.log(string);
+		console.log(str);
 	}
 }
 
 module.exports = steps;
 
 // in an interview, try to write the solution first in pseudo-code in the whiteboard before you code it, since your interviewer will be able to help you find errors.
+
+// here we have an iterative solution, and a recursive solution.
+
+// in a recursive problem, the first step is to find out about the base case, that's the case when we don't have anything else to do and we can return and stop the recursive process. we do this before anything else, since we want to avoid infinite recursion.
