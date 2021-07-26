1fba38d2ef395fdf2ef8b967f097b3d9f61f58ac
Jose Boix <bmm00000@icloud.com>
2021-07-26 15:20:35 +0100

    practice matrix exercise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -55,6 +55,48 @@ function matrix(n) {
 	//
 	//
 	//
+	const result = [];
+	for (let i = 0; i < n; i++) {
+		result.push([]);
+	}
+
+	let startCol = 0;
+	let endCol = n - 1;
+	let startRow = 0;
+	let endRow = n - 1;
+	let counter = 1;
+
+	while (startCol <= endCol && startRow <= endRow) {
+		for (let i = startCol; i <= endCol; i++) {
+			result[startRow][i] = counter;
+			counter++;
+		}
+
+		startRow++;
+
+		for (let i = startRow; i <= endRow; i++) {
+			result[i][endCol] = counter;
+			counter++;
+		}
+
+		endCol--;
+
+		for (let i = endCol; i >= startCol; i--) {
+			result[endRow][i] = counter;
+			counter++;
+		}
+
+		endRow--;
+
+		for (let i = endRow; i >= startRow; i--) {
+			result[i][startCol] = counter;
+			counter++;
+		}
+
+		startCol++;
+	}
+
+	return result;
 }
 
 module.exports = matrix;
