fd5bf3692d0238300366d2a80aa04e588736c5ca
Jose Boix <bmm00000@icloud.com>
2021-08-20 13:17:02 +0100

    add methods to linked list class

--- exercises/linkedlist/NOTES.md
+++ exercises/linkedlist/NOTES.md
@@ -1,6 +1,6 @@
 LINKED LISTS APPEAR VERY FREQUENTLY IN INTERVIEWS!
 
-a linked list is an ordered collection of data: the collection contains a number of different nodes, each node contains some amount of data, and a reference to the next node: a linked list is a list of linked nodes, or a chain or nodes. the order of the nodes will not change, unless we specifically want to change it.
+a linked list is an ordered collection of data: the collection contains a number of different nodes, each node contains some amount of data, and a reference to the next node: a linked list is a list of linked nodes, or a chain of nodes. the order of the nodes will not change, unless we specifically want to change it.
 
 in every linked list there are two special nodes: the head node, and the tail node. the tail node does not have a reference to any other node. the data contained in the node can be of any type (number, string...)
 
--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -20,6 +20,12 @@ class LinkedList {
 	}
 
 	insertFirst(data) {
+		// const oldHead = this.head;
+		// this.head = new Node(data, oldHead);
+		// or:
+		// const node = new Node(data, this.head);
+		// this.head = node;
+		// or:
 		this.head = new Node(data, this.head);
 	}
 
@@ -31,6 +37,7 @@ class LinkedList {
 			counter++;
 			node = node.next;
 		}
+
 		return counter;
 	}
 
@@ -39,212 +46,16 @@ class LinkedList {
 	}
 
 	getLast() {
-		// if (!this.head) {
-		// 	return null;
-		// }
+		let last = this.head;
 
-		// let node = this.head;
-
-		// while (node) {
-		// 	if (node.next) {
-		// 		node = node.next;
-		// 	} else {
-		// 		return node;
-		// 	}
-		// }
-
-		if (!this.head) {
-			return null;
-		}
-
-		let node = this.head;
-
-		while (node) {
-			if (!node.next) {
-				return node;
+		while (last) {
+			if (!last.next) {
+				return;
 			}
-			node = node.next;
-		}
-	}
-
-	clear() {
-		this.head = null;
-	}
-
-	removeFirst() {
-		if (!this.head) {
-			return;
-		}
-		this.head = this.head.next;
-	}
-
-	removeLast() {
-		if (!this.head) {
-			return;
+			last = last.next;
 		}
 
-		if (!this.head.next) {
-			this.head = null;
-			return;
-		}
-
-		let previous = this.head;
-		let node = this.head.next;
-
-		// while (node) {
-		// 	if (!node.next) {
-		// 		previous.next = null;
-		// 		return;
-		// 	} else {
-		// 		previous = node;
-		// 		node = node.next;
-		// 	}
-		// }
-
-		while (node.next) {
-			previous = node;
-			node = node.next;
-		}
-		previous.next = null;
-	}
-
-	insertLast(data) {
-		// if (!this.head) {
-		// 	this.head = new Node(data);
-		// 	return;
-		// }
-
-		// const lastNode = this.getLast();
-		// lastNode.next = new Node(data);
-		// return;
-
-		const lastNode = this.getLast();
-
-		if (lastNode) {
-			lastNode.next = new Node(data);
-		} else {
-			this.head = new Node(data);
-		}
-	}
-
-	getAt(integer) {
-		// if (!this.head) {
-		// 	return null;
-		// }
-
-		// const size = this.size();
-
-		// if (integer >= size) {
-		// 	return null;
-		// }
-
-		// let node = this.head;
-		// let counter = 0;
-
-		// while (node) {
-		// 	if (integer === counter) {
-		// 		return node;
-		// 	}
-
-		// 	counter++;
-		// 	node = node.next;
-		// }
-
-		// a more efficient solution:
-		let node = this.head;
-		let counter = 0;
-
-		while (node) {
-			if (integer === counter) {
-				return node;
-			}
-
-			counter++;
-			node = node.next;
-		}
-		return null;
-	}
-
-	removeAt(integer) {
-		// if (!this.head) {
-		// 	return;
-		// }
-
-		// const node = this.getAt(integer);
-		// const previousNode = this.getAt(integer - 1);
-		// const nextNode = this.getAt(integer + 1);
-
-		// if (node === this.head) {
-		// 	this.head = this.head.next;
-		// 	return;
-		// }
-
-		// previousNode.next = nextNode;
-
-		if (!this.head) {
-			return;
-		}
-
-		if (integer === 0) {
-			this.head = this.head.next;
-			return;
-		}
-
-		const previous = this.getAt(integer - 1);
-
-		if (!previous || !previous.next) {
-			return;
-		}
-		previous.next = previous.next.next;
-	}
-
-	insertAt(data, integer) {
-		// const newNode = new Node(data);
-
-		// if (!this.head) {
-		// 	this.head = newNode;
-		// 	return;
-		// }
-
-		// const previousNode = this.getAt(integer - 1);
-		// const nextNode = this.getAt(integer);
-
-		// if (!previousNode) {
-		// 	this.head = newNode;
-		// 	newNode.next = nextNode;
-		// 	return;
-		// }
-
-		// if (!nextNode) {
-		// 	const lastNode = this.getLast();
-		// 	lastNode.next = newNode;
-		// 	return;
-		// }
-
-		// previousNode.next = newNode;
-		// newNode.next = nextNode;
-
-		if (!this.head) {
-			this.head = new Node(data);
-			return;
-		}
-
-		if (integer === 0) {
-			this.head = new Node(data, this.head);
-			return;
-		}
-
-		const previous = this.getAt(integer - 1) || this.getLast();
-		const node = new Node(data, previous.next);
-		previous.next = node;
-		return;
-	}
-
-	forEach(func) {
-		for (let i = 0; i < this.size(); i++) {
-			const node = this.getAt(i);
-			func(node, i);
-		}
+		return last;
 	}
 }
 
