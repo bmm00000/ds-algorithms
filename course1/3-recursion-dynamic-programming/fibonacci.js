b7a861fa039ce25d16fddd8bcd4b74dd551da1a2
Jose Boix <bmm00000@icloud.com>
2021-12-23 17:59:38 +0100

    add dynamic programming to fib problem

--- course1/3-recursion-dynamic-programming/fibonacci.js
+++ course1/3-recursion-dynamic-programming/fibonacci.js
@@ -60,4 +60,51 @@ counter = 0;
 
 // we tested in the screenshot for 2^n, and it gives us a larger number, so in reality we might be a bit better than 2^n, but since we care about the general trend, we are definitely closer to 2^n (especially if we test this for bigger and bigger n) than we are to cubic, quadratic, etc. With experience, you will see this and you won't need to do this testing.
 
-// we found that our loop solution was better than our recursive solution, but can we still make recursion work for this problem as well? yes, we can use a technique called dynamic programming => we are going to combine recursion with memoization (memoization essentially means that we are going to store some data for the execution of a function or some other process, in our case, we will used this stored data to optimize the recursion process). so we are going to combine recursion with some data that is stored. therefore dynamic programming = recursion + memoization
+// we found that our loop solution was better than our recursive solution, but can we still make recursion work for this problem as well? yes, we can use a technique called dynamic programming => we are going to combine recursion with memoization (memoization essentially means that we are going to store some data (intermediate results) for the execution of a function or some other process, in our case, we will use this stored data to optimize the recursion process). so we are going to combine recursion with some data that is stored. therefore dynamic programming = recursion + memoization
+
+// in order to implement memoization, we could set up a global storage object, outside of the function, and we could add key value pairs to that object with each iteration, like this:
+
+const memo = {};
+
+function fib(n) {}
+
+fib(4);
+fib(5);
+
+// the problem with this approach is that we have one object, and if we call 'fib' multiple times, then we will be reusing this object. this would be great if what we want in subsequent calls is to take advange of the values that we already have in 'memo' from former calls. however, if you have a function, where the input produces totally different values for 'memo', then you would get incorrect values in your next function executions.
+
+// in order to make our approach reusable, what we are going to do is the following: every recursive process (every fibonacci number we are trying to retreive) gets its own storage which is only used for this recursive tree. for instance, in our example, fib(4) and fib(5) will use different storages. therefore, we will pass a second argument ('memo') and that will be our storage:
+
+let counter; // we add this counter to count expression executions.
+function fib(n, memo) {
+	counter++;
+	let result;
+	if (memo[n]) {
+		return memo[n];
+	}
+	if (n === 1 || n === 2) {
+		result = 1;
+	} else {
+		result = fib(n - 1, memo) + fib(n - 2, memo);
+	}
+	memo[n] = result;
+	return result;
+	// this is the same that we did before, only that now we use the 'result' variable. we do that so that we can store the results in our intermediate storage.
+}
+
+fib(5, {});
+console.log(counter);
+counter = 0;
+
+fib(6, {});
+console.log(counter);
+counter = 0;
+
+fib(20, {});
+console.log(counter);
+counter = 0;
+
+fib(50, {});
+console.log(counter);
+counter = 0;
+// see the 'counter's consoled log in the screenshot, and compare with what we had before.
