812f180a15ad7ebd5626e1eadd71cd0e0f0f6203
Jose Boix <bmm00000@icloud.com>
2022-03-26 10:58:11 +0000

    practice bianary search algo with while loop

--- course1/4-search-algorithms/ss.js
+++ course1/4-search-algorithms/ss.js
@@ -23,29 +23,50 @@
 //
 //
 
-const binarySearchLoop = (sortedArr, element) => {
-	let midIndex = Math.floor(sortedArr.length / 2);
-	let counter = 0;
-	let newArrLength = sortedArr.length;
+// const binarySearchLoop = (sortedArr, element) => {
+// 	let midIndex = Math.floor(sortedArr.length / 2);
+// 	let counter = 0;
+// 	let newArrLength = sortedArr.length;
 
-	while (element !== sortedArr[midIndex]) {
-		if (element < sortedArr[midIndex]) {
-			const newArr = sortedArr.slice(0, midIndex);
-			newArrLength = newArr.length;
-			midIndex = Math.floor(newArr.length / 2);
-		} else {
-			const newArr = sortedArr.slice(midIndex, newArrLength + counter);
-			newArrLength = newArr.length;
-			counter += midIndex;
-			midIndex = counter + Math.floor(newArr.length / 2);
-		}
-	}
+// 	while (element !== sortedArr[midIndex]) {
+// 		if (element < sortedArr[midIndex]) {
+// 			const newArr = sortedArr.slice(0, midIndex);
+// 			newArrLength = newArr.length;
+// 			midIndex = Math.floor(newArr.length / 2);
+// 		} else {
+// 			const newArr = sortedArr.slice(midIndex, newArrLength + counter);
+// 			newArrLength = newArr.length;
+// 			counter += midIndex;
+// 			midIndex = counter + Math.floor(newArr.length / 2);
+// 		}
+// 	}
 
-	return midIndex;
+// 	return midIndex;
 
-	// ONLY THE CASE WHERE WE ARE LOOKING FOR 60 DOESN'T WORK!!!
-	// sill have an infinite loop when looking for 60.
-};
+// 	// ONLY THE CASE WHERE WE ARE LOOKING FOR 60 DOESN'T WORK!!!
+// 	// sill have an infinite loop when looking for 60.
+// };
 
 // console.log(binarySearchRecursive([1, 4, 6, 30, 44, 60], 60));
 // console.log(binarySearchLoop([1, 4, 6, 30, 44, 60], 60));
+
+function findElement(sortedArr, element) {
+	let startIndex = 0;
+	let endIndex = sortedArr.length - 1;
+
+	while (startIndex <= endIndex) {
+		let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+		if (element === sortedArr[midIndex]) {
+			return midIndex;
+		}
+
+		if (sortedArr[midIndex] < element) {
+			startIndex = midIndex + 1;
+		} else {
+			endIndex = midIndex - 1;
+		}
+	}
+}
+
+// console.log(findElement([1, 4, 6, 30, 44, 60], 60));
