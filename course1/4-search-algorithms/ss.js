3cb43cdda1457f2e35ef92b28222a8a8ec2d81db
Jose Boix <bmm00000@icloud.com>
2022-03-28 11:57:52 +0100

    practice binary search loop solution

--- course1/4-search-algorithms/ss.js
+++ course1/4-search-algorithms/ss.js
@@ -21,34 +21,6 @@
 // };
 
 //
-//
-
-// const binarySearchLoop = (sortedArr, element) => {
-// 	let midIndex = Math.floor(sortedArr.length / 2);
-// 	let counter = 0;
-// 	let newArrLength = sortedArr.length;
-
-// 	while (element !== sortedArr[midIndex]) {
-// 		if (element < sortedArr[midIndex]) {
-// 			const newArr = sortedArr.slice(0, midIndex);
-// 			newArrLength = newArr.length;
-// 			midIndex = Math.floor(newArr.length / 2);
-// 		} else {
-// 			const newArr = sortedArr.slice(midIndex, newArrLength + counter);
-// 			newArrLength = newArr.length;
-// 			counter += midIndex;
-// 			midIndex = counter + Math.floor(newArr.length / 2);
-// 		}
-// 	}
-
-// 	return midIndex;
-
-// 	// ONLY THE CASE WHERE WE ARE LOOKING FOR 60 DOESN'T WORK!!!
-// 	// sill have an infinite loop when looking for 60.
-// };
-
-// console.log(binarySearchRecursive([1, 4, 6, 30, 44, 60], 60));
-// console.log(binarySearchLoop([1, 4, 6, 30, 44, 60], 60));
 
 function findElement(sortedArr, element) {
 	let startIndex = 0;
@@ -70,3 +42,24 @@ function findElement(sortedArr, element) {
 }
 
 // console.log(findElement([1, 4, 6, 30, 44, 60], 60));
+
+function binarySearch1(sortedArr, element) {
+	let startIndex = 0;
+	let endIndex = sortedArr.length - 1;
+
+	while (startIndex <= endIndex) {
+		let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+		if (element === sortedArr[midIndex]) {
+			return midIndex;
+		}
+
+		if (element < sortedArr[midIndex]) {
+			endIndex = midIndex - 1;
+		} else {
+			startIndex = midIndex + 1;
+		}
+	}
+}
+
+// console.log(binarySearch1([1, 4, 6, 30, 44, 60], 60));
