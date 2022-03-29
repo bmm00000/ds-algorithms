dbdc14335233005560a38e82b80075eab24d4290
Jose Boix <bmm00000@icloud.com>
2022-03-29 11:14:30 +0100

    practice recursive binary search

--- course1/4-search-algorithms/ss.js
+++ course1/4-search-algorithms/ss.js
@@ -17,49 +17,106 @@
 // 		counter += midIndex;
 // 	}
 
-// 	return binarySearch(newArr, element);
+// 	return binarySearchRecursive(newArr, element);
 // };
 
 //
 
-function findElement(sortedArr, element) {
-	let startIndex = 0;
-	let endIndex = sortedArr.length - 1;
+// function findElement(sortedArr, element) {
+// 	let startIndex = 0;
+// 	let endIndex = sortedArr.length - 1;
 
-	while (startIndex <= endIndex) {
-		let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+// 	while (startIndex <= endIndex) {
+// 		let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
 
-		if (element === sortedArr[midIndex]) {
-			return midIndex;
-		}
+// 		if (element === sortedArr[midIndex]) {
+// 			return midIndex;
+// 		}
 
-		if (sortedArr[midIndex] < element) {
-			startIndex = midIndex + 1;
-		} else {
-			endIndex = midIndex - 1;
-		}
+// 		if (sortedArr[midIndex] < element) {
+// 			startIndex = midIndex + 1;
+// 		} else {
+// 			endIndex = midIndex - 1;
+// 		}
+// 	}
+// }
+
+// // console.log(findElement([1, 4, 6, 30, 44, 60], 60));
+
+// function binarySearch1(sortedArr, element) {
+// 	let startIndex = 0;
+// 	let endIndex = sortedArr.length - 1;
+
+// 	while (startIndex <= endIndex) {
+// 		let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+// 		if (element === sortedArr[midIndex]) {
+// 			return midIndex;
+// 		}
+
+// 		if (element < sortedArr[midIndex]) {
+// 			endIndex = midIndex - 1;
+// 		} else {
+// 			startIndex = midIndex + 1;
+// 		}
+// 	}
+// }
+
+// // console.log(binarySearch1([1, 4, 6, 30, 44, 60], 60));
+
+// function binarySearchss(sortedArr, element) {
+// 	let startIndex = 0;
+// 	let endIndex = sortedArr.length - 1;
+
+// 	while (startIndex <= endIndex) {
+// 		const midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+// 		if (element === sortedArr[midIndex]) {
+// 			return midIndex;
+// 		}
+
+// 		if (element < sortedArr[midIndex]) {
+// 			endIndex = midIndex - 1;
+// 		} else {
+// 			startIndex = midIndex + 1;
+// 		}
+// 	}
+// }
+
+// let counter = 0;
+// function binaryRec(sortedArr, element) {
+// 	let startIndex = 0;
+// 	let endIndex = sortedArr.length - 1;
+// 	let midIndex = Math.floor((endIndex - startIndex) / 2);
+
+// 	if (element === sortedArr[midIndex]) {
+// 		return counter + midIndex;
+// 	}
+
+// 	if (element < sortedArr[midIndex]) {
+// 		const newArr = sortedArr.slice(0, midIndex);
+// 		return binaryRec(newArr, element);
+// 	} else {
+// 		const newArr = sortedArr.slice(midIndex + 1);
+// 		counter++;
+// 		return binaryRec(newArr, element);
+// 	}
+// }
+
+let counter = 0;
+function binRec(sortedArr, element) {
+	const midIndex = Math.floor(sortedArr.length / 2);
+
+	if (element === sortedArr[midIndex]) {
+		return counter + midIndex;
+	}
+
+	if (element < sortedArr[midIndex]) {
+		const newArr = sortedArr.slice(0, midIndex);
+		return binRec(newArr, element);
+	} else {
+		const newArr = sortedArr.slice(midIndex + 1);
+		counter = sortedArr.length - newArr.length;
+		return binRec(newArr, element);
 	}
 }
-
-// console.log(findElement([1, 4, 6, 30, 44, 60], 60));
-
-function binarySearch1(sortedArr, element) {
-	let startIndex = 0;
-	let endIndex = sortedArr.length - 1;
-
-	while (startIndex <= endIndex) {
-		let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
-
-		if (element === sortedArr[midIndex]) {
-			return midIndex;
-		}
-
-		if (element < sortedArr[midIndex]) {
-			endIndex = midIndex - 1;
-		} else {
-			startIndex = midIndex + 1;
-		}
-	}
-}
-
-// console.log(binarySearch1([1, 4, 6, 30, 44, 60], 60));
