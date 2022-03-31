96e717af78461bcaba1c27e6ddb755ba6432a476
Jose Boix <bmm00000@icloud.com>
2022-03-31 11:49:51 +0100

    add offset to recursive solution to binary search algo

--- course1/4-search-algorithms/ss.js
+++ course1/4-search-algorithms/ss.js
@@ -118,3 +118,48 @@ function binarySearchRec(sortedArr, element) {
 		return binarySearchRec(newArr, element);
 	}
 }
+
+function binarySearchLoop(sortedArr, element) {
+	let startIndex = 0;
+	let endIndex = sortedArr.length - 1;
+
+	while (startIndex <= endIndex) {
+		const midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+		if (element === sortedArr[midIndex]) {
+			return midIndex;
+		}
+
+		if (element < sortedArr[midIndex]) {
+			endIndex = midIndex - 1;
+		} else {
+			startIndex = midIndex + 1;
+		}
+	}
+}
+
+function binarySearchRec2(sortedArr, element, offset) {
+	let startIndex = 0;
+	let endIndex = sortedArr.length - 1;
+	let midIndex = Math.floor((endIndex - startIndex) / 2);
+
+	if (element === sortedArr[midIndex]) {
+		return offset + midIndex;
+	}
+
+	if (element < sortedArr[midIndex]) {
+		endIndex = midIndex;
+		const newArr = sortedArr.slice(startIndex, endIndex);
+		return binarySearchRec2(newArr, element, offset);
+	}
+
+	if (element > sortedArr[midIndex]) {
+		startIndex = midIndex + 1;
+		const newArr = sortedArr.slice(startIndex, endIndex + 1);
+		offset += midIndex + 1;
+		return binarySearchRec2(newArr, element, offset);
+	}
+}
+
+// solved!!!
+console.log(binarySearchRec2([1, 2, 3, 4, 5], 5, 0));
