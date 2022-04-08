47a42c632161bb63767a99c58e47654db91ea5df
Jose Boix <bmm00000@icloud.com>
2022-04-08 11:20:56 +0100

    edit asymptotic analysis notes

--- course1/3-recursion-dynamic-programming/debug/app.js
+++ course1/3-recursion-dynamic-programming/debug/app.js
@@ -1,39 +1,40 @@
+// my initial solution (non-reusable):
+
 // let count = 0;
 // const memo = {};
-// const fibRecursive = (n) => {
-// 	if (n < 0) {
-// 		return;
-// 	}
-
-// 	count++;
-
-// 	if (memo[n]) {
-// 		return memo[n];
-// 	}
-
-// 	if (n === 0 || n === 1) {
-// 		memo[n] = 1;
-// 		return memo[n];
-// 	}
-
-// 	memo[n] = fibRecursive(n - 1) + fibRecursive(n - 2);
-// 	return memo[n];
-// };
-
-// fibRecursive(10);
-// console.log(memo);
-// console.log(count);
-
-//
-//
-
-let count = 0;
-const fibRecReusable = (n, memo = {}) => {
+const fibRecursive = (n) => {
 	if (n < 0) {
 		return;
 	}
 
 	count++;
+
+	if (memo[n]) {
+		return memo[n];
+	}
+
+	if (n === 0 || n === 1) {
+		memo[n] = 1;
+		return memo[n];
+	}
+
+	memo[n] = fibRecursive(n - 1) + fibRecursive(n - 2);
+	return memo[n];
+};
+
+// fibRecursive(10);
+// console.log(memo);
+// console.log(count);
+
+// my initial solution (re-usable):
+
+// let count = 0;
+const fibRecReusable = (n, memo = {}) => {
+	if (n < 0) {
+		return;
+	}
+
+	// count++;
 
 	if (memo[n]) {
 		return memo[n];
