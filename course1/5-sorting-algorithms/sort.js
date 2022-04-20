be72efeb8ea471a5a83a30fa52f75c91d2e76df8
Jose Boix <bmm00000@icloud.com>
2022-04-20 12:54:20 +0100

    practice quick sort algo. intro to merge sort algo

--- course1/5-sorting-algorithms/sort.js
+++ course1/5-sorting-algorithms/sort.js
@@ -82,6 +82,7 @@ function bubbleSort(arr) {
 
 const sort = (arr) => {
 	const copiedArray = [...arr];
+	// QUESTION: WHY DO WE NEED TO CLONE 'arr'? I HAVE TRIED MUTATING IT (SOLUTION BELOW), AND IT WORKS.
 
 	if (copiedArray.length <= 1) {
 		return copiedArray;
@@ -120,3 +121,71 @@ const sort = (arr) => {
 
 const sortedArray = sort([-3, 10, -3, -15]);
 console.log(sortedArr);
+
+// MY SOLUTION:
+function quickSort(arr) {
+	const pivot = arr[0];
+	const smaller = [];
+	const larger = [];
+	const equal = [];
+
+	for (let num of arr) {
+		if (num === pivot) {
+			equal.push(num);
+		} else if (num < pivot) {
+			smaller.push(num);
+		} else if (num > pivot) {
+			larger.push(num);
+		}
+		// we use 'else if', because, if we use just 'if' statements, we will have unnecessary line executions.
+	}
+
+	if (smaller.length <= 1 && larger.length <= 1) {
+		return [...smaller, ...equal, ...larger];
+		// or:
+		// return smaller.concat(equal, larger);
+	}
+
+	return [...quickSort(smaller), ...equal, ...quickSort(larger)];
+	// or:
+	// return quickSort(smaller).concat(equal, quickSort(larger));
+}
+
+// MY REFACTOR OF THE COURSE'S SOLUTION:
+function quickSortCourse(arr) {
+	if (arr.length <= 1) {
+		return arr;
+	}
+
+	const pivot = arr.shift();
+	const equal = [pivot];
+	const smaller = [];
+	const larger = [];
+
+	while (arr.length) {
+		const next = arr.shift();
+		if (next === pivot) {
+			equal.push(next);
+		} else if (next < pivot) {
+			smaller.push(next);
+		} else if (next > pivot) {
+			larger.push(next);
+		}
+	}
+
+	return [...quickSortCourse(smaller), ...equal, ...quickSortCourse(larger)];
+}
+
+// look at time complexity of quick sort algo: since we are splitting the problem into pieces, it tends to be logarithmic time complexity, but the worse case scenario is that elements are already sorted (ascending or descending order): for example, if the order is ascending, the 'smaller' array will be empty, and the 'larger' array will contain the rest of the elements, so you won't split the problem into pieces, but you will rather iterate through all the elements, and with each iteration, you will have a nested iteration starting again. rule of thumb: if you call yourself inside of a function (ie. you have a recursive function), you typically have linear time complexity, but if inside that recursive function you then have a loop so you go through the entire array again, it's the same as having a nested loop in a loop, and that's quadratic time complexity.
+// in the best case scenario, we split the problem in chunks that are pretty much the same size, so we end up with logarithmic time complexity (we are roughly splitting the problem in half).
+// on average, we don't have a sorted array (if we knew the array was already sorted, we wouldn't need a sorting algo in the first place), so we can assume that the problem will be split into pretty much equal sub parts. we use the master theorem in this case again:
+// Recursive step runtime: O(n^logb(a)) => O(n^log2(2)) => O(n)
+// a (number of sub-problems, ie. how many calls we are making) => 2, because we are calling ourselves twice (see line 176).
+// b => in a random array, not sorted, it will be, roughly, also 2
+// Runtime outside of the recursion: the main thing we have is the while loop: O(n)
+// in this case, the same work is being done inside and outside of the recursion. therefore the algo runtime is O(n * log(n))
+
+// therefore, the quick sort algo, is an improvement of the bubble sort algo, in terms of performance. however, we still have quadratic time complexity in the worse case of the quick sort algo. so maybe we can do better than that:
+
+// MERGE SORT ALGO:
+// after we split the array into arrays of two elements, we have two steps: sort the arrays of two elements, and then sort them again when we merge them. therefore, in this algo, we split things up, then we sort them, and then we sort them again when we merge things together.
