221d59415af9b26c8f1af3b033edef4651bbd3e8
Jose Boix <bmm00000@icloud.com>
2022-06-27 14:32:38 +0100

    revise linked lists

--- data-structures-js/linked-lists.js
+++ data-structures-js/linked-lists.js
@@ -18,14 +18,31 @@ class LinkedList {
 	}
 
 	prepend(value) {
-		const newNode = { value: value, next: null };
+		const newNode = { value: value, next: this.head };
 
-		if (!this.head) {
-			this.head = newNode;
+		this.head = newNode;
+		// when we override the head, we are not removing the old head object from memory (it just means it's not stored in a property of the linked list): it still exists in memory. but if we don't use the old head (as we do above in line 21) the js garbage collector will get rid of it.
+
+		if (!this.tail) {
 			this.tail = newNode;
-		} else {
-			newNode.next = this.head;
-			this.head = newNode;
+		}
+	}
+
+	delete(value) {
+		if (!this.head) {
+			return;
+		}
+
+		if (this.head.value === value) {
+			this.head = this.head.next;
+		}
+
+		let curNode = this.head;
+		while (curNode.next) {
+			if (curNode.next.value === value) {
+				curNode.next = curNode.next.next;
+			}
+			curNode = curNode.next;
 		}
 	}
 
@@ -47,4 +64,8 @@ linkedList1.append(1);
 linkedList1.append('Hello');
 linkedList1.append(2);
 linkedList1.append(true);
+linkedList1.prepend('first value');
+linkedList1.delete('Hello');
+linkedList1.delete(1);
+linkedList1.delete('first value');
 console.log(linkedList1.toArray());
