d7ec44fb87dbc45f1d670430c8525f2b6a11bfdd
Jose Boix <bmm00000@icloud.com>
2022-07-30 00:33:49 +0100

    add notes on trees

--- data-structures-js/trees/trees.js
+++ data-structures-js/trees/trees.js
@@ -1,3 +1,37 @@
+// class Node {
+// 	constructor(value, parentNode = null) {
+// 		this.value = value;
+// 		this.parentNode = parentNode;
+// 		this.children = [];
+// 	}
+
+// 	addChildNode(value) {
+// 		const newNode = new Node(value, this);
+// 		this.children.push(newNode);
+// 		return { node: newNode, index: this.children.length - 1 };
+// 	}
+
+// 	deleteChildNode(index) {
+// 		this.children.splice(index, 1);
+// 	}
+// }
+
+// class Tree {
+// 	constructor(rootValue) {
+// 		this.root = new Node(rootValue);
+// 	}
+// }
+
+// const filesystem = new Tree('/');
+// const documentsData = filesystem.root.addChildNode('/documents');
+// const gamesData = filesystem.root.addChildNode('/games');
+// const resultsData = documentsData.node.addChildNode('/results');
+// const warGameData = gamesData.node.addChildNode('/wargame.exe');
+
+// console.log(filesystem);
+
+//
+
 class Node {
 	constructor(value, parentNode = null) {
 		this.value = value;
@@ -5,13 +39,13 @@ class Node {
 		this.children = [];
 	}
 
-	addChildNode(value) {
-		const newNode = new Node(value, this);
-		this.children.push(newNode);
-		return { node: newNode, index: this.children.length - 1 };
+	addNode(value) {
+		const node = new Node(value, this);
+		this.children.push(node);
+		return { node, index: this.children.length - 1 };
 	}
 
-	deleteChildNode(index) {
+	removeNode(index) {
 		this.children.splice(index, 1);
 	}
 }
@@ -23,9 +57,10 @@ class Tree {
 }
 
 const filesystem = new Tree('/');
-const documentsData = filesystem.root.addChildNode('/documents');
-const gamesData = filesystem.root.addChildNode('/games');
-const resultsData = documentsData.node.addChildNode('/results');
-const warGameData = gamesData.node.addChildNode('/wargame.exe');
+const documentsNodeInfo = filesystem.root.addNode('/documents');
+const gamesNodeInfo = filesystem.root.addNode('/games');
+
+documentsNodeInfo.node.addNode('/results.txt');
+gamesNodeInfo.node.addNode('game.exe');
 
 console.log(filesystem);
