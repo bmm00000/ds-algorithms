7439298b8de96a3be755e2df7c04663055afa1ed
Jose Boix <bmm00000@icloud.com>
2022-08-05 14:09:01 +0100

    practice trees

--- data-structures-js/trees/practice.js
+++ data-structures-js/trees/practice.js
@@ -6,9 +6,30 @@ class Node {
 	}
 
 	addNode(value) {
-		const node = new Node(value, this);
-		this.children.push(node);
-		return { node, index: this.children.length - 1 };
+		const segments = value.split('/');
+
+		if (segments.length === 0) {
+			return;
+		}
+
+		if (segments.length === 1) {
+			const node = new Node(segments[0], this);
+			this.children.push(node);
+			return { node, index: this.children.length - 1 };
+		}
+
+		const existingChildNode = this.children.find(
+			(child) => child.value === segments[0]
+		);
+
+		if (existingChildNode) {
+			this.addNode(segments.slice(1).join('/'), this);
+		} else {
+			const node = new Node(segments[0], this);
+			node.addNode(segments.slice(1).join('/'));
+			this.children.push(node);
+			return { node, index: this.children.length - 1 };
+		}
 	}
 
 	removeNode(index) {
@@ -20,4 +41,16 @@ class Tree {
 	constructor(rootValue) {
 		this.root = new Node(rootValue);
 	}
+
+	add(path) {
+		this.root.addNode(path);
+	}
 }
+
+const filesystem = new Tree('/');
+filesystem.add('documents/taxes/2020.txt');
+filesystem.add('documents/taxes/2021.txt');
+filesystem.add('games/war/cod.exe');
+filesystem.add('games/war/cod2.exe');
+
+console.log(filesystem);
